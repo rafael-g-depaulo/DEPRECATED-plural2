8dd78129a2763365edbf68e71133199e7b2b9174
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-04-27 18:58:58 -0300

    ✨ ➕ Add Strapi dep/app

---
+++ apps/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c1858201d28104f9a6d49f0ef6e436e4'),
+    },
+  },
+});
---
+++ apps/strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '855a72d5-85fe-4065-94f0-f21e1b27c960'
+};
