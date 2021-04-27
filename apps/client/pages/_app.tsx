d6c1b8bc3847128457daff0e8e21a9c2399fb358
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-04-27 19:51:05 -0300

    mess around with client

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -8,12 +8,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to client!</title>
+        <title>Welcome to plural!</title>
       </Head>
       <div className="app">
         <header className="flex">
           <NxLogo width="75" height="50" />
-          <h1>Welcome to client!</h1>
+          <h1>Welcome to plural!</h1>
         </header>
         <main>
           <Component {...pageProps} />
