ed0c250da83b92eae5ba518e01e104e363cb3933
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-04-29 00:33:44 -0300

    💩 it fucken workds

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { sum } from "utils/test"
@@ -16,9 +16,28 @@ export function Index() {
    */
   const a = 67
   const b = 2
+
+  const [animals, setAnimals] = useState([])
+
+  useEffect(() => {
+    fetch('/strapi/animals')
+      .then(a => a.json())
+      .then(a => {
+        console.log("result?", a)
+        setAnimals(a)
+        // console.log("result?", a)
+        // console.log("result?", a)
+        // console.log("result?", a)
+      })
+  }, [])
   return (
     <StyledPage>
       <p>{a} + {b} is {sum(a, b)}</p>
+      <ul>
+        {animals.map((animal, i) => (
+          <li key={animal.id}>title: {animal.title}</li>
+        ))}
+      </ul>
     </StyledPage>
   );
 }
